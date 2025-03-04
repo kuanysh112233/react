cb2611cd84b9ee99d8f5c9d897a2fa09ccd8daa7
Kuanysh <tkuanys767@gmail.com>
2025-03-04 09:49:18 +0500

    React

---
+++ test-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import './companents/ProfileCard'
+
+import ProfileCard from './companents/ProfileCard'
+
+export default function App() {
+  return (
+    <>
+    <ProfileCard/>
+    </>
+  )
+}
+
---
+++ test-app/src/companents/ProfileCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css'
+
+export default function ProfileCard() {
+    return (
+      <div>
+      <h1>Менің визит картам</h1>
+    <div className="card">
+        <p><strong>Аты-жөні:</strong> Name Surname</p>
+        <p>Мамандығы: FullStack Developer</p>
+        <p>Қала: Almaty, KZ</p>
+    </div>
+  </div>
+    )
+  }
